f047ef6750238c02c2d0b8c831bb2f192451b495
the-great-loofah <163227822+the-great-loofah@users.noreply.github.com>
2025-10-26 22:42:11 -0600

    This is the dice class
    This is the dice class, it has a d20, d8 and d4 in it. Its very simple to modify and add to if needed

---
+++ Dice.cs
@@ -0,0 +1,57 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceBattleGame
+{
+    internal abstract class Dice
+    {
+        int sides = 2;
+
+        Random roll = new Random();
+        public virtual int Roll()
+        {
+            Console.WriteLine("If youre seeing this, something has gone wrong involving the Dice class");
+            return roll.Next(1, sides);
+        }
+    }
+
+    internal class D20 : Dice
+    {
+        private int sides = 20;
+
+        Random roll = new Random();
+        public override int Roll()
+        {
+            return roll.Next(1, sides);
+        }
+    }
+
+    internal class D8 : Dice
+    {
+        private int sides = 8;
+
+        Random roll = new Random();
+
+        public override int Roll()
+        {
+            return roll.Next(1, sides);
+        }
+    }
+
+    internal class D4 : Dice
+    {
+        private int sides = 4;
+
+        Random roll = new Random();
+
+        public override int Roll()
+        {
+            return roll.Next(1, sides);
+        }
+    }
+
+
+}
