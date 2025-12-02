ea690dc05f24389e03275392defbee3f6750cc5c
calvar2828 <carlos.daa29@gmail.com>
2025-12-01 18:25:34 -0700

    Create new branch for forms manager

---
+++ Forms UI/StartMenuForm.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace DiceBattleGame
+{
+    public partial class StartMenuForm : Form
+    {
+        public StartMenuForm()
+        {
+            InitializeComponent();
+            
+        }
+
+        private void btn_StartGame_Click(object sender, EventArgs e)
+        {
+            GameManager.SwitchTo(new CharacterSelectForm());
+        }
+    }
+}
