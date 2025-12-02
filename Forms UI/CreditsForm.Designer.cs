8d13629c077f459a860c495ffe0eab75e9bc9496
calvar2828 <carlos.daa29@gmail.com>
2025-12-01 20:15:55 -0700

    Cganges 1.0

---
+++ Forms UI/CreditsForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace DiceBattleGame.Forms_UI
+{
+    partial class CreditsForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            richTextBox1 = new RichTextBox();
+            btn_StartMenuForm = new Button();
+            SuspendLayout();
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(226, 41);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.ReadOnly = true;
+            richTextBox1.Size = new Size(299, 244);
+            richTextBox1.TabIndex = 0;
+            richTextBox1.Text = "Created by:\n\n*Carlos Alvarez\n*Junaid Matyas-Hussain\n*Luis Sy\n*Nicole Ricare";
+            richTextBox1.TextChanged += richTextBox1_TextChanged;
+            // 
+            // btn_StartMenuForm
+            // 
+            btn_StartMenuForm.Location = new Point(371, 356);
+            btn_StartMenuForm.Name = "btn_StartMenuForm";
+            btn_StartMenuForm.Size = new Size(131, 40);
+            btn_StartMenuForm.TabIndex = 1;
+            btn_StartMenuForm.Text = "Start Menu";
+            btn_StartMenuForm.UseVisualStyleBackColor = true;
+            btn_StartMenuForm.Click += btn_StartMenuForm_Click;
+            // 
+            // CreditsForm
+            // 
+            AutoScaleDimensions = new SizeF(12F, 30F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(btn_StartMenuForm);
+            Controls.Add(richTextBox1);
+            Name = "CreditsForm";
+            Text = "CreditsForm";
+            ResumeLayout(false);
+        }
+
+        #endregion
+
+        private RichTextBox richTextBox1;
+        private Button btn_StartMenuForm;
+    }
+}
--- Forms UI/StartMenuForm.Designer.cs
+++ Forms UI/StartMenuForm.Designer.cs
@@ -29,11 +29,13 @@
         private void InitializeComponent()
         {
             btn_StartGame = new Button();
+            btn_CreditsForm = new Button();
+            btn_InstructionsForm = new Button();
             SuspendLayout();
             // 
             // btn_StartGame
             // 
-            btn_StartGame.Location = new Point(331, 307);
+            btn_StartGame.Location = new Point(323, 67);
             btn_StartGame.Name = "btn_StartGame";
             btn_StartGame.Size = new Size(131, 40);
             btn_StartGame.TabIndex = 0;
@@ -41,11 +43,33 @@
             btn_StartGame.UseVisualStyleBackColor = true;
             btn_StartGame.Click += btn_StartGame_Click;
             // 
+            // btn_CreditsForm
+            // 
+            btn_CreditsForm.Location = new Point(323, 227);
+            btn_CreditsForm.Name = "btn_CreditsForm";
+            btn_CreditsForm.Size = new Size(131, 40);
+            btn_CreditsForm.TabIndex = 1;
+            btn_CreditsForm.Text = "Credits";
+            btn_CreditsForm.UseVisualStyleBackColor = true;
+            btn_CreditsForm.Click += btn_CreditsForm_Click;
+            // 
+            // btn_InstructionsForm
+            // 
+            btn_InstructionsForm.Location = new Point(323, 145);
+            btn_InstructionsForm.Name = "btn_InstructionsForm";
+            btn_InstructionsForm.Size = new Size(131, 40);
+            btn_InstructionsForm.TabIndex = 2;
+            btn_InstructionsForm.Text = "Instructions";
+            btn_InstructionsForm.UseVisualStyleBackColor = true;
+            btn_InstructionsForm.Click += btn_InstructionsForm_Click;
+            // 
             // StartMenuForm
             // 
             AutoScaleDimensions = new SizeF(12F, 30F);
             AutoScaleMode = AutoScaleMode.Font;
             ClientSize = new Size(800, 450);
+            Controls.Add(btn_InstructionsForm);
+            Controls.Add(btn_CreditsForm);
             Controls.Add(btn_StartGame);
             Name = "StartMenuForm";
             Text = "StartMenuForm";
@@ -55,5 +79,7 @@
         #endregion
 
         private Button btn_StartGame;
+        private Button btn_CreditsForm;
+        private Button btn_InstructionsForm;
     }
 }
