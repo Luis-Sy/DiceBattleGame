a5cb35bf10aa87c3742f8476c5feb87e0e4d2c8f
the-great-loofah <163227822+the-great-loofah@users.noreply.github.com>
2025-12-03 00:49:04 -0700

    Framework Potions for Testing
    I am having issues modifying the character stats location inside the dictionaries, the incomplete strength potion is in here for review

---
+++ Items/StrengthPotion.cs
@@ -0,0 +1,23 @@
+﻿using DiceBattleGame.Data.Characters;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiceBattleGame.GameData.Items
+{
+    internal class StrengthPotion:Item
+    {
+        public StrengthPotion()
+        {
+            string name = "Strength Potion";
+            int price = 10;
+        }
+
+        public override void Use(Character entity) //I dont know if this even works - J
+        {
+            entity.getStats()["Strength"] = entity.getStats()["Strength"] + 5;
+        }
+    }
+}
